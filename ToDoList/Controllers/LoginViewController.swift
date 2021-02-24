c4a8fa1f6de3083fe770ae4e427164e6004e6afd
Albert <63644392+AlbertBudanov@users.noreply.github.com>
2021-02-24 22:37:13 +0300

    FirstCommit

---
+++ ToDoList/Controllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  ToDoList
+//
+//  Created by Альберт on 24.02.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+}
+
